32b2170368d8a8e7285ba3f7177bedf1bf34d347
YoannBDL <yoannb64@gmail.com>
2022-01-26 14:08:37 +0100

    Smoothier dropDowns opening/closing

--- src/components/Ui/DropDown.js
+++ src/components/Ui/DropDown.js
@@ -6,10 +6,14 @@ export const DropDown = ({ title, content, type = 'large' }) => {
 	const [isOpen, setIsOpen] = useState(false);
 
 	return (
-		<article className={'dropdown ' + (type === 'large' ? 'large' : 'min')}>
+		<article
+			className={'dropdown ' + (type === 'large' ? 'large' : 'min') + (isOpen ? '' : ' drop_closed')}
+			onClick={() => {
+				setIsOpen(!isOpen);
+			}}>
 			<div className="dropdown__title">
 				<h3>{title}</h3>
-				<img src={arrowUp} alt="Open/Close dropdown" />
+				<img src={arrowUp} alt="Open/Close dropdown" className={isOpen ? 'arr_down' : ''} />
 			</div>
 			<p className="dropdown__content">{content}</p>
 		</article>
---
+++ src/utils/useFetchData.js
@@ -0,0 +1,7 @@
+/**
+ * Fabriquer ici un hook pour récupérer les datas
+ * - Sans params pour l'ensemble des logements
+ * - Avec un param id pour récupérer la fiche d'un logement ciblé
+ *
+ * Code async pour simuler prochaine api
+ */
